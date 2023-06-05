3b2e9966883c7464bc940def4c24684c732ad887
Alena Andryiashava <alena.andryiashava@gmail.com>
2023-06-05 13:38:10 +0200

    add sign in test

--- src/main/java/Pages/SignInPage.java
+++ src/main/java/Pages/SignInPage.java
@@ -6,16 +6,17 @@ import io.qameta.allure.Step;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byCssSelector;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class SignInPage {
-    private static final SelenideElement signInForm = $x("//div[@id='signin']");
-    private static final SelenideElement emailInput = $x("//input[@id='sw-form-capture-email-input']");
-    private static final SelenideElement passwordInput = $x("//input[@id='sw-form-password-input']");
-    private static final SelenideElement signInButton = $x("//a[@id='sw-sign-in-submit-btn']");
-    private static final SelenideElement errorContainer = $(byClassName("error-message login-error d-block"));
+    private  final SelenideElement signInForm = $x("//div[@id='signin']");
+    private  final SelenideElement emailInput = $x("//input[@id='sw-form-capture-email-input']");
+    private  final SelenideElement passwordInput = $x("//input[@id='sw-form-password-input']");
+    private  final SelenideElement signInButton = $x("//a[@id='sw-sign-in-submit-btn']");
+    private  final SelenideElement forgotPasswordLink = $(byCssSelector("[href='/forgot-password']"));
+
 
 
     @Step("Authentication and authorization with correct credentials.")
@@ -27,33 +28,35 @@ public class SignInPage {
     }
 
     @Step("Check signIn form")
-    public void checkSignInForm() {
+    public SignInPage checkSignInForm() {
         signInForm.shouldBe(Condition.visible, Duration.ofSeconds(2000));
+        return this;
 
     }
 
     @Step("Enter '{email}'")
-    public void setEmail(String email) {
+    public SignInPage setEmail(String email) {
         emailInput.shouldBe(Condition.visible, Duration.ofSeconds(10)).click();
         emailInput.setValue(email);
+        return this;
 
     }
 
     @Step("Enter '{password}'")
-    public void setPassword(String password) {
+    public SignInPage setPassword(String password) {
         passwordInput.shouldBe(Condition.visible, Duration.ofSeconds(10)).click();
         passwordInput.setValue(password);
+        return this;
 
     }
     @Step("Click sign in button")
-    public HomePage signIn() {
+    public SignInPage signIn() {
         signInButton.shouldBe(Condition.visible).click();
-        return new HomePage();
+        return new SignInPage();
+    }
+    @Step(" Click  link 'Forgot password'")
+    public void forgotPassword() {
+        forgotPasswordLink.shouldBe(Condition.visible).click();
     }
 
-    @Step("Check error message")
-    public SignInPage checkError() {
-        errorContainer.shouldHave(Condition.cssClass("error"));
-        return this;
-    }
 }
--- src/test/java/ui/SignInTest.java
+++ src/test/java/ui/SignInTest.java
@@ -1,35 +1,35 @@
 package ui;
 
-import Pages.Header;
 import Pages.HomePage;
 import Pages.SignInPage;
 
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.sleep;
 
 public class SignInTest extends BaseTest{
 
-    @Test(groups = {"auth_tests"}, dataProvider = "excelCorrectDataRead", dataProviderClass = DataProviders.class)
+
+    @Test(dataProvider = "excelCorrectDataRead", dataProviderClass = DataProviders.class)
     public void correctAuthTestWithDataProviderExcel(String email, String password) {
-        sleep(2000);
-        new Header().
-                clickSignInButton();
-        new SignInPage().
-                getAuth(email, password);
-        new HomePage()
-                .checkHomePageOpeningByMessage();
+        new HomePage().clickSignInButton();
+        new SignInPage()
+                .checkSignInForm()
+                .getAuth(email, password);
+       new HomePage().checkHomePageOpeningByMessage();
+       new HomePage().signOut();
     }
 
-     @Test(testName = "CorrectAuthTest")
+    @Test(testName = "CorrectAuthTeacherTest")
     public void correctAuthTest() {
-        new Header().clickSignInButton();
-        sleep(2000);
+        String email  = "roxanne@example.com";
+        String password = "123456";
+        new HomePage().clickSignInButton();
         SignInPage signInPage = new SignInPage();
-        signInPage.checkSignInForm();
-        signInPage.setEmail("roxanne@example.com");
-        signInPage.setPassword("123456");
-        signInPage.signIn();
+        signInPage
+                .checkSignInForm()
+                .setEmail(email)
+                .setPassword(password)
+                .signIn();
         new HomePage().checkHomePageOpeningByMessage();
 
     }
